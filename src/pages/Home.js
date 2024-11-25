fe5232522ab8bda2add65c08e0f63432e1fce299
Phillyangel215 <apressley731@gmail.com>
2024-11-25 16:15:37 -0500

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+const Home = () => {
+    return <h1>Welcome to Shi Shi Kokoro Property Management</h1>;
+  };
+  
+  export default Home;
+  
---
+++ src/pages/Tenants.js
@@ -0,0 +1,5 @@
+const Tenants = () => {
+  return <h2>Tenants Page</h2>;
+};
+
+export default Tenants;
